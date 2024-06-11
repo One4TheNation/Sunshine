88f8b74580b84bc2191bdf6e4197393d7fc62f90
Navarre <jpnavarre@Navarres-iMac.local>
2024-06-11 10:47:15 -0400

    http request failure changed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,7 @@ const holderEl = document.querySelector("#holder");
 const town = document.querySelector("#town")
 
 function getLocation(cityName) {
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&units=imperial&appid=${apiKey}`
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&units=imperial&appid=${apiKey}`
 
     fetch(url).then(function (response) {
         return response.json();
