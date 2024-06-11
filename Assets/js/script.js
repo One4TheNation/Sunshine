a827893fbb549d42e68767deb19b67170f4fdeb5
Navarre <jpnavarre@Navarres-iMac.local>
2024-06-10 23:58:54 -0400

    updating javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,78 @@
-const apiKey = "8b7aeadf1b3c92eec962330f1b7c05cf";
+const apiKey = "73b41495e76b5d1053e1ce8832281325";
+let city = JSON.parse(localStorage.getItem("city"));
+let area = JSON.parse(localStorage.getItem("coords"));
+const holderEl = document.querySelector("#holder");
+const town = document.querySelector("#town")
 
-function getLocation (cityName) {
-    const url = `http://api.openweather.org/geo/1.0/direct?q=${cityName}&appid-${apikey}`;
-    
-    fetch(url).then(function (response){
+function getLocation(cityName) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+
+    fetch(url).then(function (response) {
         return response.json();
-    }).then(function(data) {
-        console.lat = data[0].lat;
+    }).then(function (data) {
+        const lat = data[0].lat;
         const lon = data[0].lon;
-        getWeather(lat, lon);
+        getWeather(lat, lon, cityName);
 
     })
 }
 
-function getWeather(lat, lon) {
-    const url = `http://api.openweather.org/data/2.5/forecast?lat=${lat}&lon-${lon}&appid-${apikey}`;
+function getWeather(lat, lon, city) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    fetch(url).then(function (response){
+    fetch(url).then(function (response) {
         return response.json();
-    }).then(function(data){
+    }).then(function (data) {
         console.log(data);
+
+        const temp = data.list[0].main.temp;
+        const wind = data.list[0].wind.speed;
+        const humidity = data.list[0].main.humidity;
+        const hOne = `
+        <div class="row-6">
+            <h2>City: ${city}</h2>
+            <h4>Temp: ${temp}° F</h4>
+            <h4>Wind: ${wind} MpH</h4>
+            <h4>Humidity: ${humidity}%</h4>
+        </div>`
+
+        $("#today").append(hOne);
+
     })
 }
 
-getLocation("")
+
+
+
+//function renderCity() {
+   // let city = JSON.parse(localStorage.getItem("city")) || []
+    //for (const town of city) {
+      //  console.log(town);
+
+       // const infoEl = `
+       // <div class="side">
+        //    <button type="button" class="list-group-item list-group-item-action">${city}</button>
+       // </div>`
+       // holderEl.innerHTML = holderEl.innerHTML + infoEl;
+   // }
+
+    //const town = {
+    //    city: city.value,
+    //};
+
+    //city.push(town);
+
+   // localStorage.setItem("city", JSON.stringify(city));
+    //location.assign('index.html') 
+//}
+//renderCity();
+
+
+
+$('#search-form').on('submit', function(event) {
+    event.preventDefault();
+
+    const cityName = $('#exampleInputLocation1').val().trim();
+
+    getLocation(cityName)
+});
